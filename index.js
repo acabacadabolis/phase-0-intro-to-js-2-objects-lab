b39e3a1cc9ca5e78142c0890ec7926f0873e59cf
Kerem Deen <Kerem.deen@gmail.com>
2023-08-08 12:52:54 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = {
+    name : "Bob",
+    streetAddress : "pokertown"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return (employee, newEmployee);
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key, value) {
+    delete employee[key];
+    return employee;
+}
